133869e4fa5a77a5eb04d33189c1e9971d82bf76
Aayushi Khandelwal <khandelwalaayushi370@gmail.com>
2026-02-06 13:17:28 +0530

    Program-1-Calculator using CLA
    This program demonstrates command line argument and type-casting.

---
+++ Code.java
@@ -0,0 +1,44 @@
+public class Code {
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        add(a,b);
+        multiply(a,b);
+        divide(a,b);
+        subtract(a,b);
+        modulus(a,b);
+    }
+    //Addition of numbers
+    public static void add(int a,int b)
+    {
+        System.out.print("The addition of the numbers is:");
+        System.out.println(a+b);
+    }
+    //Multiplication of numbers
+    public static void multiply(int a,int b)
+    {
+        System.out.print("The multiplication of the numbers is:");
+        System.out.println(a*b);
+    }
+    //Division of numbers
+    public static void divide(int a,int b)
+    {
+        System.out.print("The division of the numbers is:");
+        System.out.println(a/b);
+    }
+    //Subtraction of numbers
+    public static void subtract(int a,int b)
+    {
+        System.out.print("The subtraction of the numbers is:");
+        System.out.println(a-b);
+    }
+    //Modulus of numbers
+    public static void modulus(int a,int b)
+    {
+        System.out.print("The modulus of the numbers is:");
+        System.out.println(a%b);
+    }
+} 
+
+    
